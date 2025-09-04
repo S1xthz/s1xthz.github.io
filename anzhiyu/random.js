aa3e94d4591eea4db7cb23f9c176839213ad5912
s1xthz <598508267k@gmail.com>
2025-09-04 19:52:43 +0800

    Site updated: 2025-09-04 19:52:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/09/04/非常牛逼的hexo教程/","2025/09/04/壁纸分享/"];function toRandomPost(){
+var posts=["2025/09/04/壁纸分享/","2025/09/04/非常牛逼的hexo教程/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
