5947059acccd8109f82b1ddb564a6c92e97f7972
s1xthz <598508267k@gmail.com>
2025-09-04 11:44:00 +0800

    Site updated: 2025-09-04 11:44:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/09/04/非常牛逼的hexo教程/","2025/09/03/hello-world/"];function toRandomPost(){
+var posts=["2025/09/03/hello-world/","2025/09/04/非常牛逼的hexo教程/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
