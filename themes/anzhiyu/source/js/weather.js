93176436a459e60680f786887d9797f05da1de26
s1xthz <598508267k@gmail.com>
2025-09-05 17:35:31 +0800

    add_branch

---
+++ themes/anzhiyu/source/js/weather.js
@@ -0,0 +1,13 @@
+
+    (function(a,h,g,f,e,d,c,b){b=function(){d=h.createElement(g);c=h.getElementsByTagName(g)[0];d.src=e;d.charset="utf-8";d.async=1;c.parentNode.insertBefore(d,c)};a["SeniverseWeatherWidgetObject"]=f;a[f]||(a[f]=function(){(a[f].q=a[f].q||[]).push(arguments)});a[f].l=+new Date();if(a.attachEvent){a.attachEvent("onload",b)}else{a.addEventListener("load",b,false)}}(window,document,"script","SeniverseWeatherWidget","//cdn.sencdn.com/widget2/static/js/bundle.js?t="+parseInt((new Date().getTime() / 100000000).toString(),10)));
+    window.SeniverseWeatherWidget('show', {
+      flavor: "slim",
+      location: "WECNYQ9U02EE",
+      geolocation: true,
+      language: "auto",
+      unit: "c",
+      theme: "auto",
+      token: "ef2f14d0-5f51-4bfa-a85f-9402ec4a5698",
+      hover: "enabled",
+      container: "tp-weather-widget"
+    })
