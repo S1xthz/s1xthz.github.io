d532859c50fdfaf8bc8c57f9fa12b0a7e213d4f4
s1xthz <598508267k@gmail.com>
2025-09-05 21:00:43 +0800

    Site updated: 2025-09-05 21:00:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/09/05/初来乍到/","2025/09/04/非常牛逼的hexo教程/","2025/09/04/壁纸分享/"];function toRandomPost(){
+var posts=["2025/09/05/初来乍到/","2025/09/04/壁纸分享/","2025/09/05/转载：为什么想要改变性别的大多是男生？/","2025/09/04/非常牛逼的hexo教程/","2025/09/05/转载：县城漫展，一地鸡毛/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
